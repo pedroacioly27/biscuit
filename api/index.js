5659b451a5365de20d8833a2f75f83cee76f306a
Pedro Acioly <pedrocastroacioly27@gmail.com>
2025-06-09 20:04:21 -0300

    finish

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const serverless = require("serverless-http");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(3000);
+module.exports = app;
+module.exports.handler = serverless(app);
+
