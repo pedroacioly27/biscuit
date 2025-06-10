04b681945e1d247d0c86547102c4bc111b66bd20
Pedro Acioly <pedrocastroacioly27@gmail.com>
2025-06-09 22:14:41 -0300

    ok

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const routes = require("../routes/index");
+// const routes = require("../routes/index");
+const orders = require("./controllers/orders");
 const serverless = require("serverless-http");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(routes);
 
-app.listen(3000);
+routes.get("/", orders.getOrders);
+routes.get("/description/:id", orders.getDescriptions);
+routes.get("/title/description/:title", orders.getDescriptionsTitle);
+routes.post("/", orders.postOrder);
+routes.post("/description", orders.postDescription);
+routes.delete("/:id", orders.deleteOrders);
+routes.delete("/description/:id", orders.deleteDescription);
 
-// module.exports = app;
-// module.exports.handler = serverless(app);
+module.exports = app;
+module.exports.handler = serverless(app);
